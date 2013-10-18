237d1dd7e5214aa019fe97bc31f1f83ae67088f1
Max Walker <max@packettide.com>
2013-10-18 14:25:20 -0400

    Totally working support for containers and icons.

--- index.rb
+++ index.rb
@@ -5,23 +5,46 @@ require 'yaml'
 set :port, 9494
 set :haml, :format => :html5
 
-get '/hi' do
-  @testYaml=<<eos
-  brand: MenuStrap
-  items:
-    - url: /home
-      name: Home
-    - url: /other
-      name: Other
-      children:
-        - url: /one
-          name: One
-        - url: /two
-          name: Two
-    - url: /admin
-      name: Admin
+def haml_tag_if(condition, *args, &block)
+  if condition
+    haml_tag *args, &block
+   else
+     yield
+   end
+end
+
+get '/' do
+  @yaml=<<eos
+brand: MenuStrap
+items:
+  - url: /home
+    name: Home
+    icon: home
+  - url: /other
+    name: Other
+    children:
+      - url: /one
+        name: One
+      - url: /two
+        name: Two
+  - url: /admin
+    name: Admin
+right_items:
+  - url: /home
+    name: Home
 eos
-  @locations = YAML::load(@testYaml)['items']
-  @brand = YAML::load(@testYaml)['brand']
+  @locations = YAML::load(@yaml)['items']
+  @right_locations = YAML::load(@yaml)['right_items']
+  @brand = YAML::load(@yaml)['brand']
+  @container = false
+  haml :index
+end
+
+post '/' do
+  @yaml = params['yaml']
+  @locations = YAML::load(@yaml)['items']
+  @right_locations = YAML::load(@yaml)['right_items']
+  @brand = YAML::load(@yaml)['brand']
+  @container = YAML::load(@yaml)['container']
   haml :index
 end
